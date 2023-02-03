27730f17e5137029a60ad20c6c1ddf4bf82744c6
Parth Kataria <parth.kataria@alteryx.com>
2023-02-03 10:40:47 +0530

    Remove Headers

--- src/components/AllItems.tsx
+++ src/components/AllItems.tsx
@@ -39,8 +39,7 @@ const AllItems = () => {
             pagesize: "10",
           },
           headers: {
-            "X-RapidAPI-Key":
-              "613cf127f5msh97949d8491e5c62p1a7131jsn765e4eeb8335",
+            "X-RapidAPI-Key": "ADD KEY",
             "X-RapidAPI-Host": "apidojo-hm-hennes-mauritz-v1.p.rapidapi.com",
           },
         }
